93b6fcb2a44b8e2cbe2fc03f81ecaff6b2e402a2
hisuie08 <keisyu0825@gmail.com>
2025-12-13 23:14:07 +0900

    add path util functions

--- internal/ostools/path.go
+++ internal/ostools/path.go
@@ -1,9 +1,12 @@
 package ostools
 
 import (
+	orca "orca/helper"
 	"os"
+	"path/filepath"
 )
 
+// パスの存在確認
 func pathExists(path string, expectDir bool) bool {
 	if f, err := os.Stat(path); err == nil {
 		return f.IsDir() == expectDir
@@ -11,27 +14,79 @@ func pathExists(path string, expectDir bool) bool {
 	return false
 }
 
+// ディレクトリの存在確認
 func DirExists(path string) bool {
 	return pathExists(path, true)
 }
+
+// ファイルの存在確認
 func FileExisists(path string) bool {
 	return pathExists(path, false)
 }
 
-// サブディレクトリを走査
-func Directories(path string) []string {
+// ファイルとディレクトリを全走査
+func fileDirs(path string, isDir bool) ([]string, error) {
 	result := []string{}
-	os.Chdir(path)
-	cd, _ := os.Getwd()
-	dir, _ := os.ReadDir(cd)
-	for _, d := range dir {
-		if d.IsDir() {
-			result = append(result, cd+"/"+d.Name())
+	readDir, err := os.ReadDir(path)
+	if err != nil {
+		return nil, orca.OrcaError("directory read failed", err)
+	}
+	for _, d := range readDir {
+		if d.IsDir() != isDir {
+			continue
+		}
+		if abs, err := filepath.Abs(path); err != nil {
+			return nil, orca.OrcaError("path resolve failed", err)
+		} else {
+			dir := filepath.Join(abs, d.Name())
+			result = append(result, dir)
 		}
 	}
-	return result
+	return result, nil
 }
 
-func ToFile(content []byte, path string) error {
-	return os.WriteFile(path, content, 0775)
+// サブディレクトリを走査
+func Directories(path string) ([]string, error) {
+	return fileDirs(path, true)
+}
+
+// ディレクトリ内のファイルを走査
+func Files(path string) ([]string, error) {
+	return fileDirs(path, false)
+}
+
+// target の親ディレクトリを再帰的に作成
+func ensureParentDir(target string) error {
+	dir := filepath.Dir(target)
+	return os.MkdirAll(dir, 0o755)
+}
+
+// - target がなければ再帰的に作成
+// - content を書き込んで閉じる（既存なら上書き）
+func CreateFile(target string, content []byte) error {
+	if err := ensureParentDir(target); err != nil {
+		return err
+	}
+
+	return os.WriteFile(target, content, 0o644)
+}
+
+// - target がなければ再帰的に作成
+// - ファイル末尾に「content+改行」追記
+func AppendFile(target string, content string) error {
+	if err := ensureParentDir(target); err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(
+		target,
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+		0o644,
+	)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(content + "\n")
+	return err
 }
--- internal/ostools/path_test.go
+++ internal/ostools/path_test.go
@@ -88,3 +88,131 @@ func TestFileExisists(t *testing.T) {
 		})
 	}
 }
+
+func TestDirectories(t *testing.T) {
+	testpath = "/workspace/orca/testdata"
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		path    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"test1", testpath, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := Directories(tt.path)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("Directories() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("Directories() succeeded unexpectedly")
+			}
+			// TODO: update the condition below to compare got with tt.want.
+			if got != nil {
+				t.Logf("Directories() = %v\n", got)
+			}
+		})
+	}
+}
+
+func TestFiles(t *testing.T) {
+	testpath = "/workspace/orca/testdata"
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		path    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"test1", testpath, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := Files(tt.path)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("Files() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("Files() succeeded unexpectedly")
+			}
+			// TODO: update the condition below to compare got with tt.want.
+			if got != nil {
+				t.Logf("Files() = %v\n", got)
+			}
+		})
+	}
+}
+
+func TestAppendFile(t *testing.T) {
+	testpath = "/workspace/orca/testdata/append/append.txt"
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		target  string
+		content string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"test1", testpath, "log1", false},
+		{"test2", testpath, `multiple
+		line
+		end
+		`, false},
+		{"test3", testpath, "log3\n", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotErr := AppendFile(tt.target, tt.content)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("AppendFile() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("AppendFile() succeeded unexpectedly")
+			}
+		})
+	}
+}
+
+func TestCreateFile(t *testing.T) {
+	testpath = "/workspace/orca/testdata/create/create.txt"
+	test1, test2, test3 := "log1", `multiple
+	line
+	`, ""
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		target  string
+		content []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"test1", testpath, []byte(test1), false},
+		{"test2", testpath, []byte(test2), false},
+		{"test3", testpath, []byte(test3), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotErr := CreateFile(tt.target, tt.content)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("CreateFile() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("CreateFile() succeeded unexpectedly")
+			}
+		})
+	}
+}
