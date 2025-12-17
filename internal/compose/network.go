782ee183f254cc71f59d643fb3805c754c656895
hisuie08 <keisyu0825@gmail.com>
2025-12-17 14:13:07 +0000

    overlay関数移動

--- internal/compose/network.go
+++ internal/compose/network.go
@@ -15,16 +15,3 @@ func CollectNetworks(m map[string]*ComposeSpec) []CollectedNetwork {
 	return result
 }
 
-func ApplyNetworkOverlay(cmps *ComposeSpec, name string) {
-	for k, n := range cmps.Networks {
-		if n.Name == name {
-			// 万が一競合する同名ネットワークがcompose内にあったら削除
-			delete(cmps.Networks, k)
-		}
-	}
-	cmps.Networks["default"] = &NetworkSpec{
-		Name: name,
-		// orcaが作るか、既存のものを使うのでどちらにしろexternal
-		External: true,
-	}
-}
