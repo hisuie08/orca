22de484cbc04821d0c751f26a698ccac0b9b9744
hisuie08 <keisyu0825@gmail.com>
2025-12-29 11:01:54 +0000

    docker compose fs inspector

---
+++ internal/inspector/compose/compose.go
@@ -0,0 +1,81 @@
+package compose
+
+import (
+	"errors"
+	"orca/errs"
+	"orca/internal/context"
+	"orca/internal/inspector/fs"
+	"orca/model/compose"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+var _ ComposeInspector = (*composeInspector)(nil)
+
+type ComposeInspector interface {
+	Config(string) (*compose.ComposeSpec, error)
+}
+
+func NewInspector(root string) ComposeInspector {
+	return &composeInspector{
+		WithRoot: context.NewWithRoot(root),
+		fs:       fs.NewFsInspector,
+	}
+
+}
+
+// composeInspector
+type composeInspector struct {
+	context.WithRoot
+	fs fs.FsInspector
+}
+
+func (d *composeInspector) Config(composeDir string) (*compose.ComposeSpec, error) {
+	// HACK: 駆け上がり探索防止用の空compose
+	stopper := filepath.Join(d.Root(), "compose.yml")
+	created := false
+
+	if _, err := os.Stat(stopper); os.IsNotExist(err) {
+		os.WriteFile(stopper, []byte{}, 0644)
+		created = true
+	}
+
+	cmd := exec.Command(
+		"docker", "compose",
+		"--project-directory", composeDir,
+		"config",
+	)
+	out, err := cmd.CombinedOutput()
+
+	if created {
+		os.Remove(stopper)
+	}
+
+	if err != nil {
+		return nil, errs.ErrComposeNotFound
+	}
+	spec := &compose.ComposeSpec{}
+	if err := yaml.Unmarshal(out, spec); err != nil {
+		return nil, err
+	}
+	return spec, nil
+}
+
+func (d *composeInspector) GetAll() (compose.ComposeMap, error) {
+	result := compose.ComposeMap{}
+	dirs, err := d.fs.Dirs(d.Root())
+	if err != nil {
+		return result, &errs.FileError{Path: d.Root(), Err: err}
+	}
+	for _, dir := range dirs {
+		c, err := d.Config(dir)
+		if err != nil && errors.Is(err, errs.ErrComposeNotFound) {
+			continue
+		}
+		result[filepath.Base(dir)] = c
+	}
+	return result, nil
+}
