df5567c007d2dd03d8a8a3e3d81610c07a57f34f
hisuie08 <keisyu0825@gmail.com>
2025-12-30 03:00:03 +0000

    rename

--- internal/executor/docker/docker.go
+++ internal/executor/docker/docker.go
@@ -15,17 +15,17 @@ type Executor interface {
 	CreateVolume(string, ...string) (string, error)
 }
 
-var _ Executor = (*dockerExecutor)(nil)
+var _ Executor = (*executor)(nil)
 
 func NewExecutor(p policy.ExecPolicy) Executor {
-	return &dockerExecutor{WithPolicy: context.NewWithPolicy(p)}
+	return &executor{WithPolicy: context.NewWithPolicy(p)}
 }
 
-type dockerExecutor struct {
+type executor struct {
 	context.WithPolicy
 }
 
-func (d *dockerExecutor) run(cmd *exec.Cmd) (string, error) {
+func (d *executor) run(cmd *exec.Cmd) (string, error) {
 	if d.Policy().AllowSideEffect() {
 		out, err := cmd.CombinedOutput()
 		if err != nil {
@@ -38,26 +38,26 @@ func (d *dockerExecutor) run(cmd *exec.Cmd) (string, error) {
 	return out, nil
 }
 
-func (d *dockerExecutor) ComposeUp(composeFile string) (string, error) {
+func (d *executor) ComposeUp(composeFile string) (string, error) {
 	cmd := exec.Command("docker", "compose", "-f", composeFile, "up", "-d")
 	return d.run(cmd)
 }
 
 // docker compose down -f <file>
-func (d *dockerExecutor) ComposeDown(composeFile string) (string, error) {
+func (d *executor) ComposeDown(composeFile string) (string, error) {
 	cmd := exec.Command("docker", "compose", "-f", composeFile, "down")
 	return d.run(cmd)
 }
 
 // docker network create <name> [opt...]
-func (d *dockerExecutor) CreateNetwork(name string, opt ...string) (string, error) {
+func (d *executor) CreateNetwork(name string, opt ...string) (string, error) {
 	c := append([]string{"network", "create", name}, opt...)
 	cmd := exec.Command("docker", c...)
 	return d.run(cmd)
 }
 
 // docker volume create <name> [opt...]
-func (d *dockerExecutor) CreateVolume(name string, opt ...string) (string, error) {
+func (d *executor) CreateVolume(name string, opt ...string) (string, error) {
 	c := append([]string{"volume", "create", name}, opt...)
 	cmd := exec.Command("docker", c...)
 	return d.run(cmd)
