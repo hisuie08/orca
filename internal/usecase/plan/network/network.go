748efbf3b528a7cb39a0c0a8d1e684476d741d45
hisuie08 <keisyu0825@gmail.com>
2026-01-03 07:54:15 +0000

    network buildplan

---
+++ internal/usecase/plan/network/network.go
@@ -0,0 +1,62 @@
+package network
+
+import (
+	"orca/internal/context"
+	"orca/internal/inspector"
+	"orca/model/compose"
+	"orca/model/config"
+	"orca/model/plan"
+)
+
+type NetworkPlanContext interface {
+	context.WithConfig
+}
+
+func BuildNetworkPlan(ctx NetworkPlanContext,
+	cn []compose.CollectedNetwork) plan.NetworkPlan {
+	return buildNetworkPlan(ctx, cn, inspector.NewDocker())
+}
+
+type dockerInspector interface {
+	NetworkExists(name string) bool
+}
+
+func buildNetworkPlan(
+	ctx NetworkPlanContext,
+	cn []compose.CollectedNetwork,
+	di dockerInspector) plan.NetworkPlan {
+	cfg := ctx.Config().Network
+	np := plan.NetworkPlan{Actions: []plan.NetworkAction{}}
+	if !cfg.Enabled {
+		return np
+	}
+	np.SharedName = cfg.Name
+	exists := di.NetworkExists(cfg.Name)
+	np.Create = !exists
+	for _, net := range cn {
+		if o, ok := buildAction(net, cfg); ok {
+			np.Actions = append(np.Actions, o)
+		}
+	}
+	return np
+}
+
+func buildAction(net compose.CollectedNetwork, cfg config.ResolvedNetwork,
+) (plan.NetworkAction, bool) {
+	action := plan.NetworkAction{}
+	action.Target = plan.NetworkRef{
+		Compose: net.Ref.Compose,
+		Key:     net.Ref.Key,
+	}
+	switch {
+	case net.Ref.Key == "default" && net.Spec.Name != cfg.Name:
+		// デフォルトは上書き
+		action.ActionType = plan.NetworkOverrideDefault
+
+	case net.Ref.Key != "default" && net.Spec.Name == cfg.Name:
+		// 名前が競合するnetworkはcomposeから削除
+		action.ActionType = plan.NetworkRemoveConflict
+
+	}
+	return action, action.ActionType != ""
+}
---
+++ internal/usecase/plan/network/network_test.go
@@ -0,0 +1,97 @@
+package network
+
+import (
+	"orca/internal/context"
+	"orca/model/compose"
+	"orca/model/config"
+	"orca/model/plan"
+	"testing"
+)
+
+var _ dockerInspector = (*fakeDockerInspector)(nil)
+
+type fakeDockerInspector struct {
+	Exists string
+}
+
+func (f *fakeDockerInspector) NetworkExists(name string) bool {
+	return name == f.Exists
+}
+func fakeNetCtx(name string, enabled bool) NetworkPlanContext {
+	n := config.ResolvedNetwork{Name: name, Enabled: enabled, Internal: false}
+	ctx := context.New().WithConfig(&config.ResolvedConfig{Network: n})
+	return &ctx
+}
+func Test_CreateOrNot(t *testing.T) {
+	tests := []struct {
+		name    string
+		enabled bool
+		exists  string
+		want    bool
+	}{
+		{name: "create", enabled: true, exists: "", want: true},
+		{name: "exists", enabled: true, exists: "exists", want: false},
+		{name: "disabled", enabled: false, exists: "", want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := buildNetworkPlan(
+				fakeNetCtx(tt.name, tt.enabled),
+				[]compose.CollectedNetwork{},
+				&fakeDockerInspector{Exists: tt.exists})
+			// Create = enabled && !exists
+			if got.Create != tt.want {
+				t.Errorf("incorrect decisions Create should be %t but got %t",
+					tt.want, got.Create)
+			}
+		})
+	}
+}
+
+func fakeNetwork(c, k, n string) compose.CollectedNetwork {
+	return compose.CollectedNetwork{
+		Ref:  compose.FromRef{Compose: c, Key: k},
+		Spec: &compose.NetworkSpec{Name: n}}
+}
+
+func TestActionType(t *testing.T) {
+	netname := "shared"
+	tests := []struct {
+		name    string
+		network compose.CollectedNetwork
+		want    plan.NetworkActionType
+	}{
+		{name: "overlay", network: fakeNetwork("", "default", ""),
+			want: plan.NetworkOverrideDefault},
+		{name: "remove", network: fakeNetwork("", "custom", "shared"),
+			want: plan.NetworkRemoveConflict},
+		{name: "no-op", network: fakeNetwork("", "default", "shared")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := buildNetworkPlan(
+				fakeNetCtx(netname, true),
+				[]compose.CollectedNetwork{tt.network},
+				&fakeDockerInspector{})
+			if tt.name == "no-op" {
+				if len(got.Actions) > 0 {
+					t.Fatalf("no-op should not be registerd")
+				}
+			} else {
+				if len(got.Actions) != 1 {
+					t.Fatalf("1 action should be in but %d in actions",
+						len(got.Actions))
+				}
+				a := got.Actions[0]
+				if a.Target.Key != tt.network.Ref.Key ||
+					a.Target.Compose != tt.network.Ref.Compose {
+					t.Fatalf("incorrect target: %+v", a.Target)
+				}
+				if a.ActionType != tt.want {
+					t.Fatalf("expected action was %s but got %s",
+						tt.want, a.ActionType)
+				}
+			}
+		})
+	}
+}
