35753bd06d8f8a6802eb3e7544da3b6387466b8e
hisuie08 <keisyu0825@gmail.com>
2026-01-05 03:17:47 +0000

    overlay

---
+++ internal/usecase/compose/overlay/overlay.go
@@ -0,0 +1,46 @@
+package overlay
+
+import (
+	"orca/internal/context"
+	"orca/internal/usecase/compose/overlay/network"
+	"orca/internal/usecase/compose/overlay/volume"
+	"orca/model/compose"
+	"orca/model/plan"
+)
+
+var _ composeOverlayer = (*overlayer)(nil)
+
+type composeOverlayer interface {
+	OverlayVolume([]plan.VolumePlan)
+	OverlayNetwork(plan.NetworkPlan)
+	ComposeMap() compose.ComposeMap
+}
+
+type OverlayContext interface {
+	context.WithConfig
+}
+
+type overlayer struct {
+	ctx OverlayContext
+	cm  compose.ComposeMap
+}
+
+func ComposeOverlayer(ctx OverlayContext, cm compose.ComposeMap) *overlayer {
+	return &overlayer{cm: cm, ctx: ctx}
+}
+
+func (o *overlayer) OverlayVolume(vps []plan.VolumePlan) {
+	if o.ctx.Config().Volume.VolumeRoot != nil {
+		volume.OverlayVolume(o.cm, vps)
+	}
+}
+
+func (o *overlayer) OverlayNetwork(np plan.NetworkPlan) {
+	if o.ctx.Config().Network.Enabled {
+		network.OverlayNetwork(o.cm, np)
+	}
+}
+
+func (o *overlayer) ComposeMap() compose.ComposeMap {
+	return o.cm
+}
---
+++ internal/usecase/compose/overlay/overlay_test.go
@@ -0,0 +1,76 @@
+package overlay
+
+import (
+	"orca/internal/context"
+	"orca/model/compose"
+	"orca/model/config"
+	"orca/model/plan"
+	"testing"
+)
+
+var (
+	cRef    = "a"
+	vRef    = "a_vol"
+	nRef    = "default"
+	volRoot = "testroot"
+	netName = "testnet"
+)
+
+func fakeMap() compose.ComposeMap {
+	return compose.ComposeMap{cRef: &compose.ComposeSpec{
+		Volumes: compose.VolumesSection{
+			vRef: &compose.VolumeSpec{Driver: "local", Name: vRef}},
+		Networks: compose.NetworksSection{
+			nRef: &compose.NetworkSpec{Name: cRef + "_net"}},
+	}}
+}
+func fakeCtx(volume *string, network bool) OverlayContext {
+	ctx := context.New().WithConfig(&config.ResolvedConfig{
+		Volume:  config.ResolvedVolume{VolumeRoot: volume},
+		Network: config.ResolvedNetwork{Enabled: network, Name: netName}})
+	return &ctx
+}
+
+func fakePlan(cfg config.ResolvedConfig) ([]plan.VolumePlan, plan.NetworkPlan) {
+	vp := []plan.VolumePlan{{UsedBy: []plan.VolumeRef{
+		{Compose: cRef, Key: vRef}}, Type: plan.VolumeExternal}}
+	np := plan.NetworkPlan{SharedName: cfg.Network.Name,
+		Actions: []plan.NetworkAction{{Target: plan.NetworkRef{
+			Compose: cRef, Key: nRef}, ActionType: plan.NetworkOverrideDefault}}}
+	return vp, np
+}
+
+func TestOverlay(t *testing.T) {
+	testCases := []struct {
+		name string
+		ctx  OverlayContext
+		want func(compose.ComposeMap) bool
+	}{
+		{name: "enabled", ctx: fakeCtx(&volRoot, true),
+			want: func(nm compose.ComposeMap) bool {
+				nv := nm[cRef].Volumes[vRef]
+				nn := nm[cRef].Networks[nRef]
+				return nv.External && nv.Driver == "" &&
+					nn.Name == netName && nn.External
+			}},
+		{name: "disabled", ctx: fakeCtx(nil, false),
+			want: func(nm compose.ComposeMap) bool {
+				nv := nm[cRef].Volumes[vRef]
+				nn := nm[cRef].Networks[nRef]
+				return !nv.External && nv.Driver == "local" &&
+					nn.Name == cRef+"_net" && !nn.External
+			}},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			cm := fakeMap()
+			o := ComposeOverlayer(tt.ctx, cm)
+			vps, np := fakePlan(*tt.ctx.Config())
+			o.OverlayNetwork(np)
+			o.OverlayVolume(vps)
+			if !tt.want(cm) {
+				t.Error("overlay failed")
+			}
+		})
+	}
+}
