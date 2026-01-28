984e004294fe1c95dd765f21975c073ebde8c696
hisuie08 <keisyu0825@gmail.com>
2026-01-28 12:04:03 +0000

    add logger util

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"io"
 	"orca/internal/context"
 	. "orca/model/policy/log"
@@ -27,5 +28,11 @@ func (l *Logger) chkPolicy(lv LogLevel) bool {
 func (l *Logger) Log(lv LogLevel, p []byte) {
 	if l.chkPolicy(lv) {
 		l.out.Write(p)
+	}
+}
+
+func (l *Logger) Logln(lv LogLevel, s string) {
+	if l.chkPolicy(lv) {
+		l.out.Write([]byte(fmt.Sprintf("%s\n", s)))
 	}
 }
