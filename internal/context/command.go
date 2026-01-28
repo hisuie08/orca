31eee1c50046366fab054071bb4db43699d69ad8
hisuie08 <keisyu0825@gmail.com>
2026-01-28 10:13:47 +0000

    log policy

--- internal/context/command.go
+++ internal/context/command.go
@@ -1,9 +1,9 @@
 package context
 
 import (
-	"io"
 	"orca/cmd/baseflag"
-	"orca/internal/logger"
+	"orca/model/policy"
+	"orca/model/policy/log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,6 +12,7 @@ import (
 type CommandContext interface {
 	WithRoot
 	WithLog
+	WithPolicy
 }
 
 func BuildCommandCtx(cmd cobra.Command) CommandContext {
@@ -21,35 +22,29 @@ func BuildCommandCtx(cmd cobra.Command) CommandContext {
 	}
 	silent, _ := cmd.Flags().GetBool(baseflag.Silent)
 	debug, _ := cmd.Flags().GetBool(baseflag.Debug)
-	logLevel := func() logger.LogLevel {
+	dry, _ := cmd.Flags().GetBool(baseflag.DryRun)
+	logLevel := func() log.LogLevel {
 		if silent {
-			return logger.LogSilent
+			return log.LogSilent
 		} else if debug {
-			return logger.LogDebug
+			return log.LogDebug
 		} else {
-			return logger.LogNormal
+			return log.LogNormal
 		}
 	}()
-	ctx := New().WithRoot(wd).WithLog(logLevel, cmd.OutOrStdout())
+	plcy := func() policy.ExecPolicy {
+		if dry {
+			return policy.Dry
+		}
+		return policy.Real
+	}()
+	ctx := New().WithRoot(wd).WithPolicy(plcy).
+		WithLog(logLevel, cmd.OutOrStdout())
 	return &ctx
 }
 
 func FromCommandCtx(ctx CommandContext) Context {
-	c := New().WithRoot(ctx.Root()).WithLog(ctx.LogLevel(), ctx.LogTarget())
+	c := New().WithRoot(ctx.Root()).WithPolicy(ctx.Policy()).
+		WithLog(ctx.LogLevel(), ctx.LogTarget())
 	return c
-}
-
-type silentWriter struct{}
-
-func (s *silentWriter) Write([]byte) (int, error) {
-	return 0, nil
-}
-
-func GetWriter(out io.Writer, disablef ...bool) io.Writer {
-	for _, b := range disablef {
-		if b {
-			return &silentWriter{}
-		}
-	}
-	return out
 }
